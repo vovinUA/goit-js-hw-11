86a176675081942b8556b9f4a955270faceabd6e
vovinUA <vovin_tool@yahoo.com>
2023-03-01 15:50:39 +0200

    JS HW 11 with AOS

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ const lightbox = new SimpleLightbox('.gallery a');
 
 let page = 1;
 let searchQuery = '';
+let index = 0;
 
 formEl.addEventListener('submit', onSearch);
 loadMoreBtn.addEventListener('click', onLoadMore);
@@ -62,16 +63,16 @@ async function fetchImages(query, page) {
             <div class="photo-card">
               <a href="${largeImageURL}" class="photo-card__link">
                 <div class="photo-card__wrapper">
-                  <div class="photo-card__container">
+                  <div class="photo-card__container" data-aos="fade-down" data-aos-delay="${index * 50}">
                     <img class="photo-card__image" src="${webformatURL}" alt="${tags}" loading="lazy" />
                   </div>
                 </div>
               </a>
               <div class="info">
-                <p class="info__item"><b>Likes</b> ${likes}</p>
-                <p class="info__item"><b>Views</b> ${views}</p>
-                <p class="info__item"><b>Comments</b> ${comments}</p>
-                <p class="info__item"><b>Downloads</b> ${downloads}</p>
+                <p class="info__item" data-aos="fade-left" data-aos-delay="${index * 25}"><b>Likes</b> ${likes}</p>
+                <p class="info__item" data-aos="fade-left" data-aos-delay="${index * 50}"><b>Views</b> ${views}</p>
+                <p class="info__item" data-aos="fade-right" data-aos-delay="${index * 50}"><b>Comments</b> ${comments}</p>
+                <p class="info__item" data-aos="fade-right" data-aos-delay="${index * 25}"><b>Downloads</b> ${downloads}</p>
               </div>
             </div>
           `,
